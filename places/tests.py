659ea3ee421d77cc606b0e7b11bf13f89a6a506f
Christoph Buchetmann <chr@buchetmann.de>
2018-10-15 13:53:47 +0200

    work on test cases add login/logout

--- places/tests.py
+++ places/tests.py
@@ -214,46 +214,28 @@ class EditPlace(TestCase):
         Price.objects.create(place_id=1)
         Price.objects.create(place_id=1)
         self.std_data = {'form-TOTAL_FORMS': ['1'], 'form-INITIAL_FORMS': ['1'], 'form-MIN_NUM_FORMS': ['0'],
-                         'form-MAX_NUM_FORMS': ['1'], 'form-0-name': ['Hostel Dalmatia'],
-                         'form-0-contact_first_name': ['Dean'], 'form-0-contact_last_name': ['Johnston'],
-                         'form-0-contact_type': ['PO'], 'form-0-street': ['Put Borka 20'], 'form-0-city': ['Marusici'],
-                         'form-0-country': ['CR'], 'form-0-address_add': [''], 'form-0-phone': ['+385 95 733 5841'],
+                         'form-MAX_NUM_FORMS': ['1'], 'form-0-name': ['TestCase'], 'form-0-contact_first_name': [''],
+                         'form-0-contact_last_name': ['owner'], 'form-0-contact_type': ['NA'], 'form-0-street': [''],
+                         'form-0-city': [''], 'form-0-country': [''], 'form-0-address_add': [''], 'form-0-phone': [''],
                          'form-0-mobile': [''], 'form-0-email': ['hosttheway@gmail.com'], 'form-0-email_alt': [''],
-                         'form-0-languages': ['EN'], 'form-0-who_lives_here': ['My wife and myself'],
-                         'form-0-rooms': ['4'], 'form-0-beds': ['22'], 'form-0-maximum_of_guests': ['25'],
-                         'form-0-bathrooms': ['3'], 'form-0-private_bathroom': ['on'], 'form-0-common_kitchen': ['on'],
-                         'form-0-outdoor_place': ['on'], 'form-0-room_add': [''], 'form-0-smoking': ['on'],
-                         'form-0-pets': ['on'], 'form-0-family': ['on'], 'form-0-meals': ['BD'],
-                         'form-0-vegetarian': ['on'], 'form-0-vegan': ['on'], 'form-0-meal_example': ['Spaghetti'],
-                         'form-0-laundry': ['on'], 'form-0-parking': ['on'], 'form-0-wifi': ['on'],
-                         'form-0-description': [''], 'form-0-picture': [''], 'form-0-longitude': ['43.4024023'],
-                         'form-0-latitude': ['16.8337883'], 'form-0-max_stay': ['365'], 'form-0-min_stay': ['1'],
-                         'form-0-currencies': ['€'], 'form-0-check_out_time': ['12'], 'form-0-check_in_time': ['14'],
-                         'form-0-pick_up_service': ['on'], 'form-0-id': ['1'], 'price-TOTAL_FORMS': ['4'],
-                         'price-INITIAL_FORMS': ['4'], 'price-MIN_NUM_FORMS': ['0'], 'price-MAX_NUM_FORMS': ['3'],
-                         'price-0-place': ['1'], 'price-0-description': [''], 'price-0-value': ['12.00'],
-                         'price-0-currency': ['EUR'], 'price-0-category': ['CN'], 'price-0-valid_from': ['2018-01-01'],
-                         'price-0-valid_to': ['2018-12-31'], 'price-0-id': ['1'], 'price-1-place': ['1'],
-                         'price-1-description': [''], 'price-1-value': ['5.20'], 'price-1-currency': ['EUR'],
-                         'price-1-category': ['BR'], 'price-1-valid_from': ['2018-01-01'],
-                         'price-1-valid_to': ['2018-12-31'], 'price-1-id': ['2'], 'price-2-place': ['1'],
-                         'price-2-description': [''], 'price-2-value': ['12.00'], 'price-2-currency': ['EUR'],
-                         'price-2-category': ['CN'], 'price-2-valid_from': ['2018-01-01'],
-                         'price-2-valid_to': ['2018-12-31'], 'price-2-id': ['3'], 'price-3-place': ['1'],
-                         'price-3-description': [''], 'price-3-value': ['2.00'], 'price-3-currency': ['EUR'],
-                         'price-3-category': ['CL'], 'price-3-valid_from': ['2018-01-01'],
-                         'price-3-valid_to': ['2018-12-31'], 'price-3-id': ['5'], 'room-TOTAL_FORMS': ['2'],
-                         'room-INITIAL_FORMS': ['2'], 'room-MIN_NUM_FORMS': ['0'], 'room-MAX_NUM_FORMS': ['1'],
-                         'room-0-place': ['1'], 'room-0-room_number': ['01'], 'room-0-beds': ['5'],
-                         'room-0-price_per_person': ['20.00'], 'room-0-price_per_room': ['40.00'],
+                         'form-0-languages': ['EN'], 'form-0-who_lives_here': [''], 'form-0-rooms': ['1'],
+                         'form-0-beds': ['2'], 'form-0-maximum_of_guests': ['1'], 'form-0-bathrooms': ['1'],
+                         'form-0-room_add': [''], 'form-0-pets': ['on'], 'form-0-family': ['on'],
+                         'form-0-meals': ['NO'], 'form-0-meal_example': [''], 'form-0-wifi': ['on'],
+                         'form-0-description': [''], 'form-0-picture': [''], 'form-0-longitude': ['43.511005555555556'],
+                         'form-0-latitude': ['16.444283333333335'], 'form-0-max_stay': ['365'],
+                         'form-0-min_stay': ['1'], 'form-0-currencies': ['€'], 'form-0-check_out_time': ['12'],
+                         'form-0-check_in_time': ['14'], 'form-0-id': ['1'], 'price-TOTAL_FORMS': ['1'],
+                         'price-INITIAL_FORMS': ['0'], 'price-MIN_NUM_FORMS': ['0'], 'price-MAX_NUM_FORMS': ['3'],
+                         'price-0-place': ['1'], 'price-0-description': [''], 'price-0-value': ['0.0'],
+                         'price-0-currency': ['EUR'], 'price-0-category': ['CL'], 'price-0-valid_from': ['2018-01-01'],
+                         'price-0-valid_to': ['2018-12-31'], 'price-0-id': [''], 'room-TOTAL_FORMS': ['1'],
+                         'room-INITIAL_FORMS': ['0'], 'room-MIN_NUM_FORMS': ['0'], 'room-MAX_NUM_FORMS': ['1'],
+                         'room-0-place': ['1'], 'room-0-room_number': ['01'], 'room-0-beds': ['2'],
+                         'room-0-price_per_person': ['0.0'], 'room-0-price_per_room': ['0.0'],
                          'room-0-currency': ['EUR'], 'room-0-valid_from': ['2018-01-01'],
-                         'room-0-valid_to': ['2018-12-31'], 'room-0-bathroom': ['on'], 'room-0-outdoor_place': ['on'],
-                         'room-0-room_add': [''], 'room-0-smoking': ['on'], 'room-0-pets': ['on'],
-                         'room-0-family': ['on'], 'room-0-id': ['1'], 'room-1-place': ['1'],
-                         'room-1-room_number': ['01'], 'room-1-beds': ['2'], 'room-1-price_per_person': ['20.00'],
-                         'room-1-price_per_room': ['50.00'], 'room-1-currency': ['EUR'],
-                         'room-1-valid_from': ['2018-01-01'], 'room-1-valid_to': ['2018-12-31'],
-                         'room-1-bathroom': ['on'], 'room-1-room_add': [''], 'room-1-id': ['3'], 'id': ['1']}
+                         'room-0-valid_to': ['2018-12-31'], 'room-0-bathroom': ['on'], 'room-0-room_add': [''],
+                         'room-0-pets': ['on'], 'room-0-family': ['on'], 'room-0-id': ['']}
 
     def test_setup(self):
         response = self.client.get('/places/1/')
@@ -275,4 +257,4 @@ class EditPlace(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, '/places/1/')
         place = Place.objects.get(pk=1)
-        self.assertEqual(place.name, 'Da')
+        self.assertEqual(place.name, 'TestCase')
