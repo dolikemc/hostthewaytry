d2c5cec10224baf4ce633b5f0ad0739218ca9531
Christoph Buchetmann <chr@buchetmann.de>
2018-11-13 12:19:17 +0100

    use user admin and templates from django

--- participant/admin.py
+++ participant/admin.py
@@ -2,19 +2,21 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Traveller
+from .models import Worker
 
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
-class TravellerInline(admin.StackedInline):
-    model = Traveller
-    can_delete = False
+class WorkerInline(admin.StackedInline):
+    model = Worker
+
+    def get_max_num(self, request, obj=None, **kwargs):
+        return 1
 
 
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
-    inlines = (TravellerInline,)
+    inlines = (WorkerInline,)
 
 
 # Re-register UserAdmin
--- participant/models.py
+++ participant/models.py
@@ -2,7 +2,24 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Traveller(models.Model):
+class Participant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    street = models.CharField(max_length=64, blank=True, null=True)
+    zip = models.CharField(max_length=32, blank=True, null=True)
+    city = models.CharField(max_length=64, blank=True, null=True)
+    phone = models.CharField(max_length=64, blank=True, null=True)
+    mobile = models.CharField(max_length=64, blank=True, null=True)
+    deleted = models.BooleanField(default=False)
+
+
+class Traveller(Participant):
     picture = models.ImageField()
-    phone = models.CharField(max_length=64)
+    expiry_date = models.DateField(blank=True, null=True)
+
+
+class PlaceAdmin(Participant):
+    alt_email = models.EmailField(blank=True, null=True)
+
+
+class Worker(Traveller):
+    pass
