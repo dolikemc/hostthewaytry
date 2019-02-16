4b6f18d2e5055f27842e58d3ee0c58d27bc5f089
Christoph Buchetmann <chr@buchetmann.de>
2019-02-16 14:39:29 +0100

    test coverage 88 files 96 lines

--- tests/traveller_tests/test_views.py
+++ tests/traveller_tests/test_views.py
@@ -1,14 +1,38 @@
 from django.http.response import HttpResponse
+from django.shortcuts import reverse
 
 from tests.base import BaseTest
 from traveller.models import User
 
 
 class ModelTest(BaseTest):
-    def test_login(self):
+    def test_login_staff(self):
         self.set_up_staff()
         response: HttpResponse = self.client.post('/traveller/login/', self.credentials)
         self.assertRedirects(response, '/admin/places/place/?deleted__exact=0&reviewed__exact=0')
+
+    def test_login_staff_get(self):
+        response: HttpResponse = self.client.get('/traveller/login/')
+        self.assertTemplateUsed(response, 'traveller/login.html')
+
+    def test_register_staff_get(self):
+        response: HttpResponse = self.client.get('/traveller/register/')
+        self.assertTemplateUsed(response, 'traveller/register.html')
+
+    def test_login_place_admin(self):
+        self.set_up_place_admin()
+        response: HttpResponse = self.client.post('/traveller/login/', self.credentials)
+        self.assertRedirects(response, reverse('places:place_admin'))
+
+    def test_login_worker(self):
+        self.set_up_worker()
+        response: HttpResponse = self.client.post('/traveller/login/', self.credentials)
+        self.assertRedirects(response, reverse('places:worker'))
+
+    def test_login_traveller(self):
+        self.set_up_traveller()
+        response: HttpResponse = self.client.post('/traveller/login/', self.credentials)
+        self.assertRedirects(response, reverse('places:index'))
 
     def test_not_active_user(self):
         self.set_up_staff()
@@ -43,3 +67,10 @@ class ModelTest(BaseTest):
         self.assertTrue(self.client.login(**self.credentials))
         response: HttpResponse = self.client.get(f'/traveller/update/user/{user.id}/')
         self.assertContains(response, 'another@user.com')
+
+    def test_register_with_wrong_password(self):
+        self.set_up_staff()
+        response: HttpResponse = self.client.post('/traveller/register/', data={'email': 'ab@b.com',
+                                                                                'password1': 'zzzrtzt',
+                                                                                'password2': 'zzzztzt'})
+        self.assertContains(response, 'id_password')
