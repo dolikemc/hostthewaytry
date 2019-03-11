51549e61a38b51d26cc15c8c46eeb22fc0201511
Christoph Buchetmann <chr@buchetmann.de>
2019-03-11 07:41:02 +0100

    coverage 100/95

--- tests/utils_tests/test_wsgi.py
+++ tests/utils_tests/test_wsgi.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
+import hosttheway.wsgi as WSGI
+
 
 class WsgiTest(TestCase):
     def test_wsgi_load(self):
-        pass
+        self.assertTrue(WSGI.get_wsgi_application())
